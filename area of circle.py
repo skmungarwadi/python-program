24aae98ae0f141ca85720c1639a2d1629dfa9978
skmungarwadi <sakshimungarwadi798@gmail.com>
2026-08-31 15:03:43 +0530

    Add python program

---
+++ area of circle.py
@@ -0,0 +1,5 @@
+#from math import 
+#write a python program to find area of circle
+r=2
+area=3.15*r*r
+print("area of circle",area)
---
+++ swapping program.py
@@ -0,0 +1,12 @@
+#swapping of two no's
+a=20
+b=30
+print("Before swap")
+print("a=",a)
+print("b=",b)
+a,b=b,a
+print("After swap")
+print("a=",a)
+print("b=",b)
+
+
